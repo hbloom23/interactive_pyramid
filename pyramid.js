d6433f8227abcf1898009f470f37df2f15154961
Haley Bloom <77748052+hbloom23@users.noreply.github.com>
2021-10-27 13:15:40 -0400

    Update pyramid.js

--- pyramid.js
+++ pyramid.js
@@ -2,32 +2,28 @@
 
 var canvas;
 var gl;
-
+var theta = [0, 0, 0];
+var thetaLoc;
+var angle;
+var key;
 var points = [];
 var colors = [];
 
-var axis = 0;
-var theta = [0, 0, 0];
-var angle; 
-
-var thetaLoc;
-
-var key;
-
-const vertices = [
-    vec4(-0.5, -0.5, 0.5, 1.0),
-    vec4(0.0, 0.5, 0.0, 1.0),
-    vec4(0.5, -0.5, 0.5, 1.0),
-    vec4(0.5, -0.5, -0.5, 1.0),
-    vec4(-0.5, -0.5, -0.5, 1.0)
+const vertices = [ 
+   vec4(-0.5, -0.5, 0.5, 1.0),
+   vec4(0.0, 0.5, 0.0, 1.0),
+   vec4(0.5, -0.5, 0.5, 1.0),
+   vec4(0.5, -0.5, -0.5, 1.0),
+   vec4(-0.5, -0.5, -0.5, 1.0) 
 ];
 
 const vertexColors = [
-    [1.0, 0.0, 0.0, 1.0],
-    [1.0, 1.0, 0.0, 1.0],
-    [0.0, 1.0, 0.0, 1.0],
-    [0.0, 0.0, 1.0, 1.0],
-    [1.0, 0.0, 1.0, 1.0]
+    [ 1.0, 0.0, 0.0, 1.0 ], // red
+    [ 1.0, 1.0, 0.0, 1.0 ], // yellow
+    [ 0.0, 1.0, 0.0, 1.0 ], // green
+    [ 0.0, 0.0, 1.0, 1.0 ], // blue
+    [ 1.0, 0.0, 1.0, 1.0 ], // magenta
+
 ];
 
 window.onload = function init() {
@@ -40,6 +36,7 @@ window.onload = function init() {
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
     gl.enable(gl.DEPTH_TEST);
+
     colorCube();
 
     lines();
@@ -69,80 +66,84 @@ window.onload = function init() {
     document.getElementById("y-axis").value = 0;
     document.getElementById("z-axis").value = 0;
 
-    update();
+    update();   
 }
 
-window.onkeydown = function(event){
-    key = event.KeyCode;
+window.onkeydown = function(event) {
+    key = event.keyCode;
     keyboard(key);
 }
 
-function keyboard(key){
-    if(key == 87){
+function keyboard(key) {
+
+    if (key === 87) {
         angle = parseInt(document.getElementById("x-axis").value);
         angle = angle + 15;
         theta[0] = angle;
         document.getElementById("x-axis").value = angle;
     }
-
-    if(key == 65){
+    
+    if (key === 65) {
         angle = parseInt(document.getElementById("y-axis").value);
         angle = angle - 15;
         theta[1] = angle;
         document.getElementById("y-axis").value = angle;
     }
-
-    if(key == 83){
+    
+    if (key === 83) {
         angle = parseInt(document.getElementById("x-axis").value);
         angle = angle - 15;
         theta[0] = angle;
         document.getElementById("x-axis").value = angle;
     }
-
-    if(key == 68){
+    
+    if (key === 68) {
         angle = parseInt(document.getElementById("y-axis").value);
-        angle = angle + 15;
+        angle = angle + 15
         theta[1] = angle;
         document.getElementById("y-axis").value = angle;
     }
-
-    if(key == 81){
+    
+    if (key === 81) {
         angle = parseInt(document.getElementById("z-axis").value);
         angle = angle - 15;
         theta[2] = angle;
         document.getElementById("z-axis").value = angle;
     }
-
-    if(key == 69){
+    
+    if (key === 69) {
         angle = parseInt(document.getElementById("z-axis").value);
         angle = angle + 15;
         theta[2] = angle;
         document.getElementById("z-axis").value = angle;
-    }
-
-    if(key == 82){
+    } 
+    
+    if (key === 82) {
         reset();
     }
-
-    if(key == 13){
+    
+    if (key == 13) {
         apply();
-    }
+    } 
 }
 
-function reset(){
+function reset() {
+
     document.getElementById("x-axis").value = 0;
     document.getElementById("y-axis").value = 0;
     document.getElementById("z-axis").value = 0;
     theta = [0, 0, 0];
 }
 
-function apply(){
+function apply() {
+
     theta[0] = parseInt(document.getElementById("x-axis").value);
     theta[1] = parseInt(document.getElementById("y-axis").value);
     theta[2] = parseInt(document.getElementById("z-axis").value);
 }
 
-function lines(){
+function lines() {
+
     points.push(vec4(0.0, 0.0, 0.0, 1.0));
     colors.push(vec4(1.0, 0.0, 0.0, 1.0));
     points.push(vec4(1.0, 0.0, 0.0, 1.0));
@@ -159,32 +160,36 @@ function lines(){
     colors.push(vec4(0.0, 0.0, 1.0, 1.0));
 }
 
-function colorCube(){
-    quad( 0, 2, 3, 4);
+function colorCube() {
+    
+    quad(0, 2, 3, 4);
     triangle(0, 2, 1);
     triangle(2, 3, 1);
-    triangle( 3,4, 1);
+    triangle(3, 4, 1);
     triangle(4, 0, 1);
+    
 }
 
-function triangle(a, b, c){
+function triangle(a, b, c) {
+
     var indices = [a, b, c];
 
-    for(var i = 0; i <  indices.length; ++i){
+    for (var i = 0; i < indices.length; ++i) {
         points.push(vertices[indices[i]]);
         colors.push(vertexColors[a]);
     }
 }
 
-function quad(a, b, c, d){
-    var indices = [a, b, c, a, c, d];
-    for( var i = 0; i < indices.length; ++i){
-        points.push(vertices[indices[i]]);
+function quad(a, b, c, d) {
+
+    var indices = [ a, b, c, a, c, d ];
+    for (var i = 0; i < indices.length; ++i) {
+        points.push( vertices[indices[i]] );
         colors.push(vertexColors[1]);
     }
 }
 
-function update(){
+function update() {
     gl.uniform3fv(thetaLoc, theta);
     render();
     window.requestAnimationFrame(update);
@@ -193,5 +198,6 @@ function update(){
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLES, 0, points.length - 6);
-    gl.drawArrays(gl.LINES, points.length - 6, 6);
+    gl.drawArrays(gl.LINES, points.length - 6 , 6);  
 }
+
